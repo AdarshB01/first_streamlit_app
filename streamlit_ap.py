66ee3114450557f0aabb4bcf9772c2390609086c
Adarsh Bhadoria <123145151+AdarshB01@users.noreply.github.com>
2023-08-14 12:43:15 +0530

    Update streamlit_ap.py
    Copy content

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -5,3 +5,7 @@ streamlit.title('My Parents new healthy dinner')
 streamlit.header('Menu for the Dinner')
 streamlit.text('What will you have')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
