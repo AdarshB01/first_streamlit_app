7d11b85c99dcbb890ccd840da9c1b604bab8aedf
Adarsh Bhadoria <123145151+AdarshB01@users.noreply.github.com>
2023-08-14 12:49:51 +0530

    Update streamlit_ap.py
    Included panda

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -1,7 +1,5 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
-
-
 streamlit.header('Menu for the Dinner')
 streamlit.text('What will you have')
 
@@ -11,3 +9,7 @@ streamlit.text(' 🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
